8fc2cc7ad619c7738d59217ec7d4a9dc366af0ce
NJDaeger <noahdaeger@hotmail.com>
2017-06-04 23:43:19 -0400

    Basic stuff

---
+++ src/main/java/com/coalesce/coperms/CoPerms.java
@@ -0,0 +1,16 @@
+package com.coalesce.coperms;
+
+import com.coalesce.plugin.CoPlugin;
+
+public final class CoPerms extends CoPlugin {
+	
+	@Override
+	public void onPluginEnable() {
+	
+	}
+	
+	@Override
+	public void onPluginDisable() {
+	
+	}
+}
