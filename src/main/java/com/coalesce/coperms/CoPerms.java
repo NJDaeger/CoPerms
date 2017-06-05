aaba332befbd2b7d7a3fceb57a67ff0d7855c5f6
NJDaeger <noahdaeger@hotmail.com>
2017-06-05 08:04:58 -0400

    Need to work on JsonConfig

--- src/main/java/com/coalesce/coperms/CoPerms.java
+++ src/main/java/com/coalesce/coperms/CoPerms.java
@@ -1,12 +1,16 @@
 package com.coalesce.coperms;
 
+import com.coalesce.coperms.configuration.Configuration;
 import com.coalesce.plugin.CoPlugin;
 
 public final class CoPerms extends CoPlugin {
 	
 	@Override
 	public void onPluginEnable() {
-	
+		this.updateCheck("Project-Coalesce", "CoPerms", true);
+		
+		new Configuration(this);
+		
 	}
 	
 	@Override
---
+++ src/main/java/com/coalesce/coperms/configuration/Configuration.java
@@ -0,0 +1,19 @@
+package com.coalesce.coperms.configuration;
+
+import com.coalesce.config.json.JsonConfig;
+import com.coalesce.plugin.CoPlugin;
+
+public final class Configuration extends JsonConfig {
+	
+	public Configuration(CoPlugin plugin) {
+		super("config", plugin);
+		
+		addEntry("test.this.config", true);
+		addEntry("test.this.config2", false);
+		addEntry("test.this2.config", true);
+		addEntry("test.this2.config2", false);
+		
+		backup();
+	}
+	
+}
