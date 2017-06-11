f8015d161b3398cb85902d9014fe3100e44bac22
NJDaeger <noahdaeger@hotmail.com>
2017-06-11 10:03:03 -0400

    Made Common package and removed duplicated code.

--- src/main/java/com/coalesce/coperms/configuration/CoPermsConfig.java
+++ src/main/java/com/coalesce/coperms/configuration/CoPermsConfig.java
@@ -8,14 +8,10 @@ public final class CoPermsConfig extends YamlConfig {
 	public CoPermsConfig(CoPlugin plugin) {
 		super("config", plugin);
 		
-		
-		
 		addEntry("test.this.config", true);
 		addEntry("test.this.config2", false);
 		addEntry("test.this2.config", true);
 		addEntry("test.this2.config2", false);
-		
-		backup();
 	}
 	
 }
